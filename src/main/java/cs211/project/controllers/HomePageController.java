9e746955ae0123b452fde0dbeea634896783b00a
Kawin Sinthopsiriporn <kawin.si@ku.th>
2023-08-08 20:39:37 +0700

    finish fxml organizer manage

---
+++ src/main/java/cs211/project/controllers/HomePageController.java
@@ -0,0 +1,57 @@
+package cs211.project.controllers;
+
+import cs211.project.services.FXRouter;
+import javafx.fxml.FXML;
+
+import java.io.IOException;
+
+public class HomePageController {
+    @FXML
+    protected void onCreateClick() {
+        try {
+            FXRouter.goTo("create-event");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML
+    protected void onJoinClick() {
+        try {
+            FXRouter.goTo("event-list");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML
+    protected void onProfileClick() {
+        try {
+            FXRouter.goTo("profile-setting");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML
+    protected void onJoinedHistoryClick() {
+        try {
+            FXRouter.goTo("joined-history");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML
+    protected void onParticipantScheduleClick() {
+        try {
+            FXRouter.goTo("participant-schedule");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @FXML
+    protected void onTeamScheduleClick() {
+        try {
+            FXRouter.goTo("team-schedule");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
