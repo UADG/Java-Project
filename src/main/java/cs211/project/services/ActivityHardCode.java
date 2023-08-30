9c5f5d9a2cf32aa58a5c6a66b188047a985c04f2
B1GDAWG0 <lerdphipat.k@ku.th>
2023-08-30 20:28:39 +0700

    fix ActivityHardCode

--- src/main/java/cs211/project/services/ActivityHardCode.java
+++ src/main/java/cs211/project/services/ActivityHardCode.java
@@ -4,29 +4,31 @@ import java.util.ArrayList;
 
 public class ActivityHardCode implements Datasource<ArrayList<ArrayList<String>>>{
     @Override
-    public void writeData(ArrayList<ArrayList<String>> data) {
+    public ArrayList<ArrayList<String>> readData() {
         ArrayList<ArrayList<String>> arr = new ArrayList<>();
         ArrayList<String> a1,a2;
         a1 = new ArrayList<>();
         a1.add("activity1");
+        a1.add("18");
+        a1.add("14:00");
+        a1.add("15:30");
         a1.add("team1");
         a1.add("participant1");
-        a1.add("2022-10-7 13:00");
-        a1.add("2022-10-8 00:00");
-        a1.add("0");
         a1.add("0");
         a2 = new ArrayList<>();
         a2.add("activity2");
+        a2.add("19");
+        a2.add("11:00");
+        a2.add("12:00");
         a2.add("team2");
         a2.add("participant2");
-        a1.add("2022-10-8 13:00");
-        a1.add("2022-10-9 14:00");
         a2.add("0");
-        a2.add("0");
+
+        return arr;
     }
 
     @Override
-    public ArrayList<ArrayList<String>> readData() {
-        return null;
+    public void writeData(ArrayList<ArrayList<String>> data) {
+        
     }
 }
