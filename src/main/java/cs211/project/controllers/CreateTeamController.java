c40fb62122c82f869f19fec3752d43982717b2f5
B1GDAWG0 <lerdphipat.k@ku.th>
2023-09-12 21:19:24 +0700

    updateTeamInActivity1.0

--- src/main/java/cs211/project/controllers/CreateTeamController.java
+++ src/main/java/cs211/project/controllers/CreateTeamController.java
@@ -1,11 +1,114 @@
 package cs211.project.controllers;
 
+import cs211.project.models.Activity;
+import cs211.project.models.Team;
+import cs211.project.models.collections.ActivityList;
 import cs211.project.services.FXRouter;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 
 import java.io.IOException;
+import java.time.LocalTime;
 
 public class CreateTeamController {
+    @FXML public TableView activityTableView;
+    @FXML public Label nameLabel;
+    @FXML public Label timeStartLabel;
+    @FXML public Label timeStopLabel;
+    @FXML public TextField teamNameTextField;
+    @FXML public TextField numberOfTeamMemberTextField;
+    @FXML public Label errorLabel;
+    public ActivityList list;
+    public Activity selectedActivity;
+    public void initialize(){
+        errorLabel.setText("");
+        clearInfo();
+        list = (ActivityList) FXRouter.getData();
+        showTable(list);
+        activityTableView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Activity>() {
+            @Override
+            public void changed(ObservableValue observable, Activity oldValue, Activity newValue) {
+                if (newValue == null) {
+                    clearInfo();
+                    selectedActivity = null;
+                } else {
+                    showInfo(newValue);
+                    selectedActivity = newValue;
+                }
+            }
+        });
+    }
+
+    public void clearInfo(){
+        nameLabel.setText("");
+        timeStopLabel.setText("");
+        timeStopLabel.setText("");
+    }
+
+    public void showInfo(Activity activity){
+        nameLabel.setText(activity.getActivityName());
+        timeStartLabel.setText(activity.getStartTimeActivity());
+        timeStopLabel.setText(activity.getEndTimeActivity());
+    }
+
+    public void showTable(ActivityList list){
+        // กำหนด column ให้มี title ว่า ID และใช้ค่าจาก attribute id ของ object Student
+        TableColumn<Activity, String> activityNameColumn = new TableColumn<>("Activity Name");
+        activityNameColumn.setCellValueFactory(new PropertyValueFactory<>("activityName"));
+
+        TableColumn<Activity, String> dateActivityColumn = new TableColumn<>("Date");
+        dateActivityColumn.setCellValueFactory(new PropertyValueFactory<>("date"));
+
+        // กำหนด column ให้มี title ว่า Name และใช้ค่าจาก attribute name ของ object Student
+        TableColumn<Activity, LocalTime> startTimeActivityColumn = new TableColumn<>("Start-Time");
+        startTimeActivityColumn.setCellValueFactory(new PropertyValueFactory<>("startTimeActivity"));
+
+        // กำหนด column ให้มี title ว่า Score และใช้ค่าจาก attribute score ของ object Student
+        TableColumn<Activity, LocalTime> endTimeActivityColumn = new TableColumn<>("End-Time");
+        endTimeActivityColumn.setCellValueFactory(new PropertyValueFactory<>("endTimeActivity"));
+
+        // ล้าง column เดิมทั้งหมดที่มีอยู่ใน table แล้วเพิ่ม column ใหม่
+        activityTableView.getColumns().clear();
+        activityTableView.getColumns().add(activityNameColumn);
+        activityTableView.getColumns().add(dateActivityColumn);
+        activityTableView.getColumns().add(startTimeActivityColumn);
+        activityTableView.getColumns().add(endTimeActivityColumn);
+
+        activityTableView.getItems().clear();
+
+        // ใส่ข้อมูล Student ทั้งหมดจาก studentList ไปแสดงใน TableView
+        for (Activity activity: list.getActivities()) {
+            if(activity.getTeam() == null){
+                activityTableView.getItems().add(activity);
+            }
+        }
+    }
+
+    public void createTeam(){
+        String teamName = teamNameTextField.getText();
+        String numberStr = numberOfTeamMemberTextField.getText();
+        if(!teamName.equals("")&&!numberStr.equals("")){
+            try {
+                int number = Integer.parseInt(numberStr);
+                Team team = new Team(teamName, number);
+                selectedActivity.addTeamInActivity(team);
+                showTable(list);
+                teamNameTextField.clear();
+                numberOfTeamMemberTextField.clear();
+            }catch (NumberFormatException e){
+                errorLabel.setText("Number of people must be number");
+            }
+        }else{
+            errorLabel.setText("Must fill all information before create team");
+        }
+    }
+
     @FXML
     protected void onBackClick(){
         try {
