c847c299bf249d43c2e27d0d3ff1a52ea0793c64
Lerdphipat Kanjanarungrot <bkaosas@gmail.com>
2023-10-12 22:34:33 +0700

    create animate in application

--- src/main/java/cs211/project/cs211661project/HelloApplication.java
+++ src/main/java/cs211/project/cs211661project/HelloApplication.java
@@ -18,6 +18,7 @@ public class HelloApplication extends Application {
 
     private static void configRoute() {
         String viewPath = "cs211/project/views/";
+        FXRouter.setAnimationType("fade",200.00);
         FXRouter.when("login-page", viewPath + "login-page.fxml");
         FXRouter.when("profile-setting", viewPath + "profile-setting.fxml");
         FXRouter.when("re-password", viewPath + "re-password.fxml");
